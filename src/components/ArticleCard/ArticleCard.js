4aaec2c9eb4fd2db7f42bb0ff611a290fa86f65e
Sydney Bear <sbear3915@gmail.com>
2021-09-13 14:28:07 -0600

    Update alt text for image

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -5,9 +5,9 @@ const ArticleCard = ({title, image, section}) => {
     <section className='article-card'>
       <div className='article-header'>
         <h1 className='article-title'>{title}</h1>
-        <p className='article-section'>Section: {section}</p>
+        <p className='article-section'><em>{section}</em></p>
       </div>
-      <img className='article-image' src={image.url}/>
+      <img className='article-image' alt='article-image' src={image.url}/>
       <button className='info-button'>Click for more info</button>
     </section>
   )
