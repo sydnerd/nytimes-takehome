121a93c8dc0c86720940f691cc8f4fb5a8da1d6c
Sydney Bear <sbear3915@gmail.com>
2021-09-14 20:28:33 -0600

    Link home button back to home page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import './Nav.css'
 import sectionTypes from '../../utils/sections'
 import Select from 'react-select'
+import { Link } from 'react-router-dom'
 
 const Nav = ({updateSection}) => {
 
@@ -8,12 +9,14 @@ const options = sectionTypes.map(section => ({label: section, value: section}))
 
   return (
     <nav>
-      <h1 className='title'>News</h1>
-      <Select
-      className='drop-down'
-      options={options}
-      onChange={(event) => updateSection(event.value)}
-      />
+      <Link to ='/'><button>home</button> </Link>
+        <h1 className='title'>News</h1>
+        <Select
+        className='drop-down'
+        options={options}
+        onChange={(event) => updateSection(event.value)}
+        />
+        
     </nav>
   )
 }
